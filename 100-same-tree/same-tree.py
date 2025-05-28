7638ff22159fdc327bfc9a054ffc28f60f2a6e3d
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-05-28 12:39:37 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (86.37%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -15,24 +15,29 @@ class Solution(object):
             return True
         if not p or not q:
             return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
 
-        q1 = [p]
-        q2 = [q]
-        while q1 and q2:
-            node1 = q1.pop(0)
-            node2 = q2.pop(0)
+        # q1 = [p]
+        # q2 = [q]
+        # while q1 and q2:
+        #     node1 = q1.pop(0)
+        #     node2 = q2.pop(0)
 
-            if not node1 and not node2:
-                continue
-            if not node1 or not node2:
-                return False
-            if node1.val != node2.val:
-                return False
+        #     if not node1 and not node2:
+        #         continue
+        #     if not node1 or not node2:
+        #         return False
+        #     if node1.val != node2.val:
+        #         return False
 
-            q1.append(node1.left)
-            q2.append(node2.left)
-            q1.append(node1.right)
-            q2.append(node2.right)
+        #     q1.append(node1.left)
+        #     q2.append(node2.left)
+        #     q1.append(node1.right)
+        #     q2.append(node2.right)
 
-        return not q1 and not q2
+        # return not q1 and not q2
                 
