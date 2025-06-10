397df76ec3495121b271318678c9c42d0acc3e22
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-06-10 13:35:37 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (51.24%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        dp = [0]*(n+1)
+        dp[0], dp[1] = 1, 1
+        for i in range(2, n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[j-1] * dp[i-j]
+        return dp[n]
+        #recursion O[3^n] : bcz 3 operations each node : left, right, center
+        if n <=1:
+            return 1
+        count = 0
+        for i in range(1, n+1):
+            count+= self.numTrees(i-1) * self.numTrees(n-i)
+        return count
+        
