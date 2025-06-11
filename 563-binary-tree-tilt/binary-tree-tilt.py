7b28a47cce46fb30144cc19ef2b06ebeb0aeff90
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-06-11 13:29:23 -0700

    Time: 11 ms (30.26%) | Memory: 15.2 MB (73.33%) - LeetSync

--- 563-binary-tree-tilt/binary-tree-tilt.py
+++ 563-binary-tree-tilt/binary-tree-tilt.py
@@ -10,6 +10,33 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        def postorder(node):
+            if not node:
+                return 0, 0
+            left_sum, left_tilt = postorder(node.left)
+            right_sum, right_tilt = postorder(node.right)
+            tilt = abs(left_sum - right_sum)
+            total_tilt = tilt + left_tilt + right_tilt
+            return left_sum + right_sum + node.val , total_tilt
+        
+        tilt_sum = postorder(root)[1]
+        return tilt_sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         self.tilt_sum = 0
         def postorder(node):
             if not node:
