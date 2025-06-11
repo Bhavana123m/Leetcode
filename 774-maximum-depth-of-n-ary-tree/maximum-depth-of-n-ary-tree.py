1d28dcae1e18ef9fcf801a1eb964c054611a6121
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-06-11 12:05:30 -0700

    Time: 34 ms (44.37%) | Memory: 15.6 MB (45.42%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        heights = []
+        for node in root.children:
+            heights.append(self.maxDepth(node))
+        return max(heights) + 1
