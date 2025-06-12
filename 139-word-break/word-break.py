081aa0030c9e3a9d8810fbbb60a6bb7d8aa6b8af
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-06-11 20:43:24 -0700

    Time: 5 ms (62.54%) | Memory: 12.5 MB (42.13%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -5,10 +5,11 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
+        word_set = set(wordDict)  
         dp = [False] * (len(s)+1)
         dp[len(s)] = True
         for i in range(len(s)-1, -1, -1):
-            for word in wordDict:
+            for word in word_set:
                 if (i + len(word)) <= len(s) and s[i : i+len(word)] == word:
                     dp[i] = dp [i+len(word)]
                 if dp[i]:
