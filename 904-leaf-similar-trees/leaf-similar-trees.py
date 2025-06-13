b592b4b25f4938b28e165ef1eb47aeaf5e15e2d3
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-06-12 17:35:10 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (70.87%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: bool
+        """
+        tree_leafs1=[]
+        tree_leafs2=[]
+        self.postorder(root1, tree_leafs1)
+        self.postorder(root2, tree_leafs2)
+        len1 = len(tree_leafs1)
+        len2 = len(tree_leafs2)
+        if len1 != len2:
+            return False
+        for i in range(len1):
+            if tree_leafs1[i] != tree_leafs2[i]:
+                return False
+        return True
+
+
+    def postorder(self, node, leaf):
+        if not node:
+            return
+        if not node.left and not node.right:
+            leaf.append(node.val)
+        self.postorder(node.left, leaf)
+        self.postorder(node.right, leaf)
+
