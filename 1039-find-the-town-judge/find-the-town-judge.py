4c7c113d7c77c70b92e17feb5cb6f677e27d811d
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-06-25 16:44:51 -0700

    Time: 15 ms (76.04%) | Memory: 16.1 MB (28.25%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,115 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        indegree = [0]*(n+1)
+        outdegree = [0] * (n+1)
+
+        for a,b in trust:
+            outdegree[a]+=1
+            indegree[b]+=1
+        
+        for i in range(1,n+1):
+            if outdegree[i] == 0 and indegree[i] == n-1:
+                return i
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        indegree = [0] * (n+1)
+        outdegree = [0] * (n+1)
+        for a, b in trust:
+            outdegree[a] += 1
+            indegree[b] += 1
+        for i in range(1, n+1):
+            if indegree[i] == n-1 and outdegree[i] == 0:
+                return i
+        return -1
+        # if len(trust) == 1 and trust[0][1] == n:
+        #     return n
+        # trust_count = [0]*(n+1)
+        # for (a, b) in trust:
+        #     trust_count[a]-=1
+        #     trust_count[b]+=1
+        # print(trust_count)
+        # for judge in range(1, len(trust_count)):
+        #     if trust_count[judge] == n-1:
+        #         return judge
+        # return -1
+
