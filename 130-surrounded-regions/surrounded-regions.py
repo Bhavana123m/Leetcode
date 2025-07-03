a6e9da7d2436f6f6814e61b8771c911a3439c710
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-02 18:45:01 -0700

    Time: 7 ms (82.54%) | Memory: 16 MB (91.92%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -4,88 +4,107 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        # if not board:
-        #     return board
-        # rows = len(board)
-        # cols = len(board[0])
-
-        # queue = deque()
-
-        # for row in range(rows):
-        #     if board[row][0] == 'O':
-        #         deque.append()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         if not board:
             return board
         rows = len(board)
         cols = len(board[0])
 
-        def dfs(row, col):
-            if row<0 or row>=rows or col<0 or col>=cols or board[row][col] != 'O':
-                return
-            board[row][col] = '#'
-            dfs(row+1, col)
-            dfs(row-1, col)
-            dfs(row, col+1)
-            dfs(row, col-1)
+        queue = deque()
 
         for row in range(rows):
             if board[row][0] == 'O':
-                dfs(row,0)
+                queue.append((row, 0))
             if board[row][cols-1] == 'O':
-                dfs(row, cols-1)
+                queue.append((row, cols-1))
         for col in range(cols):
             if board[0][col] == 'O':
-                dfs(0, col)
+                queue.append((0, col))
             if board[rows-1][col] == 'O':
-                dfs(rows-1, col)
-        
+                queue.append((rows-1, col))
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        while queue:
+            row, col = queue.popleft()
+            board[row][col] = '#'
+            for dr, dc in directions:
+                new_row =row+dr
+                new_col = col+dc
+                if 0<=new_row<rows and 0<=new_col<cols and board[new_row][new_col] != '#' and board[new_row][new_col] == 'O':
+                    queue.append((new_row, new_col))
         for row in range(rows):
             for col in range(cols):
                 if board[row][col] == 'O':
-                    board[row][col] ='X'
+                    board[row][col] = 'X'
                 elif board[row][col] == '#':
-                    board[row][col] ='O'
+                    board[row][col] = 'O'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not board:
+        #     return board
+        # rows = len(board)
+        # cols = len(board[0])
+
+        # def dfs(row, col):
+        #     if row<0 or row>=rows or col<0 or col>=cols or board[row][col] != 'O':
+        #         return
+        #     board[row][col] = '#'
+        #     dfs(row+1, col)
+        #     dfs(row-1, col)
+        #     dfs(row, col+1)
+        #     dfs(row, col-1)
+
+        # for row in range(rows):
+        #     if board[row][0] == 'O':
+        #         dfs(row,0)
+        #     if board[row][cols-1] == 'O':
+        #         dfs(row, cols-1)
+        # for col in range(cols):
+        #     if board[0][col] == 'O':
+        #         dfs(0, col)
+        #     if board[rows-1][col] == 'O':
+        #         dfs(rows-1, col)
+        
+        # for row in range(rows):
+        #     for col in range(cols):
+        #         if board[row][col] == 'O':
+        #             board[row][col] ='X'
+        #         elif board[row][col] == '#':
+        #             board[row][col] ='O'
 
