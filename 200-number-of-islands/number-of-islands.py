a4607d62d50d93d5df6244534c48a86de515ff62
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-02 19:19:38 -0700

    Time: 225 ms (65.05%) | Memory: 27.4 MB (99.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,6 +5,47 @@ class Solution(object):
         :rtype: int
         """
         if not grid:
+            return grid
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+        def bfs(row, col):
+            queue = deque()
+            queue.append((row, col))
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+            while queue:
+                row, col = queue.popleft()
+                if grid[row][col] == '0':
+                    continue
+                grid[row][col] = '2'
+                for dr, dc in directions:
+                    new_row = row+dr
+                    new_col = col+dc
+                    if 0<=new_row<rows and 0<=new_col<cols and grid[new_row][new_col]!='2' and grid[new_row][new_col]=='1':
+                        grid[new_row][new_col] = '2'
+                        queue.append((new_row,new_col))
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '1':
+                    count+=1
+                    bfs(row, col)
+        return count
+                    
+                    
+        
+
+
+
+
+
+
+
+
+
+
+        
+        if not grid:
             return 0
         rows = len(grid)
         cols = len(grid[0])
