de276e654f7bcaf4ff43db88c5a4fade1d84f215
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-02 19:51:33 -0700

    Time: 3 ms (88.23%) | Memory: 12.5 MB (15.17%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -4,6 +4,117 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        if not grid or not grid[0]:
+            return -1
+
+        rows, cols = len(grid), len(grid[0])
+        queue = deque()
+        fresh = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+        if fresh == 0:
+            return 0
+        time = 0
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        queue.append((nr, nc))
+            if queue:
+                time += 1
+        return time if fresh == 0 else -1
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         n = len(grid)
         m = len(grid[0])
         fresh = 0
