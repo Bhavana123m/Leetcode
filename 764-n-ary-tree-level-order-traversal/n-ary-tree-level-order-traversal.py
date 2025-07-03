da180a39c702e3156e071287c8d52e9881635957
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-03 16:53:11 -0700

    Time: 35 ms (55.90%) | Memory: 15.7 MB (35.38%) - LeetSync

--- 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -5,13 +5,48 @@ class Node(object):
         self.val = val
         self.children = children
 """
-
+from collections import deque
+import math
 class Solution(object):
     def levelOrder(self, root):
         """
         :type root: Node
         :rtype: List[List[int]]
         """
+        # if not root:
+        #     return []
+        # max_per_level_list = []
+        # queue = deque()
+        # queue.append(root)
+        # while queue:
+        #     q_len = len(queue)
+        #     max_node_level = float('-inf')
+        #     for _ in range(q_len):
+        #         node = queue.popleft()
+        #         max_node_level = max(node.val, max_node_level)
+        #         if node.children:
+        #             for child in node.children:
+        #                 queue.append(child)
+        #     max_per_level_list.append(max_node_level)
+        # return max_per_level_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if not root:
             return []
         queue = deque()
