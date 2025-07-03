fffae140fe91aaf1a904a56874292e9667f25e35
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-03 00:38:47 -0700

    Time: 51 ms (55.62%) | Memory: 86.6 MB (59.18%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -10,26 +10,36 @@ class Solution(object):
         :type k: int
         :rtype: Optional[ListNode]
         """
-        dummyNode = ListNode(0)
-        length = 0
-        curr = head
-        while curr:
-            length+=1
-            curr = curr.next
-        first_node_pointer = k
-        second_node_pointer = length-k+1
-        count = 0
-        first_node = None
-        second_node = None
-        curr = head
-        while curr:
-            count+=1
-            if count == first_node_pointer:
-                first_node = curr
-            if count == second_node_pointer:
-                second_node = curr
-            curr = curr.next
-        if first_node and second_node:
-            first_node.val, second_node.val = second_node.val, first_node.val
-
+        right = head
+        for _ in range(k-1):
+            right = right.next
+        swap_node = right
+        left = head
+        while right.next:
+            left = left.next
+            right = right.next
+        left.val, swap_node.val = swap_node.val, left.val
         return head
+        # dummyNode = ListNode(0)
+        # length = 0
+        # curr = head
+        # while curr:
+        #     length+=1
+        #     curr = curr.next
+        # first_node_pointer = k
+        # second_node_pointer = length-k+1
+        # count = 0
+        # first_node = None
+        # second_node = None
+        # curr = head
+        # while curr:
+        #     count+=1
+        #     if count == first_node_pointer:
+        #         first_node = curr
+        #     if count == second_node_pointer:
+        #         second_node = curr
+        #     curr = curr.next
+        # if first_node and second_node:
+        #     first_node.val, second_node.val = second_node.val, first_node.val
+
+        # return head
