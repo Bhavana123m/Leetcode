394cb6b304b9bfb615fcd3aa02c3927745341373
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-03 17:28:46 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (73.83%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,25 +10,44 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        min_heap = []
-        if not list1 and not list2:
-            return None
-        if list1 and list2:
-            heapq.heappush(min_heap,(list1.val, list1))
-            heapq.heappush(min_heap,(list2.val, list2))
-        elif list1 and not list2:
-            heapq.heappush(min_heap,(list1.val, list1))
-        elif not list1 and list2:
-            heapq.heappush(min_heap,(list2.val, list2))
+        head1 = list1
+        head2 = list2
         result = ListNode(0)
-        curr = result
-        while min_heap:
-            val, node = heapq.heappop(min_heap)
-            curr.next = node
-            curr = curr.next
-            if node.next:
-                heapq.heappush(min_heap,(node.next.val, node.next))
+        current  =  result
+        while head1 and head2:
+            if head1.val > head2.val:
+                current.next = head2
+                head2 = head2.next
+            else:
+                current.next = head1
+                head1 = head1.next
+            current = current.next
+        if head1:
+            current.next = head1
+        if head2:
+            current.next = head2
+        
         return result.next
+
+        # min_heap = []
+        # if not list1 and not list2:
+        #     return None
+        # if list1 and list2:
+        #     heapq.heappush(min_heap,(list1.val, list1))
+        #     heapq.heappush(min_heap,(list2.val, list2))
+        # elif list1 and not list2:
+        #     heapq.heappush(min_heap,(list1.val, list1))
+        # elif not list1 and list2:
+        #     heapq.heappush(min_heap,(list2.val, list2))
+        # result = ListNode(0)
+        # curr = result
+        # while min_heap:
+        #     val, node = heapq.heappop(min_heap)
+        #     curr.next = node
+        #     curr = curr.next
+        #     if node.next:
+        #         heapq.heappush(min_heap,(node.next.val, node.next))
+        # return result.next
             
         
         
