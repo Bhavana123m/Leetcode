42ebec439ae13f6c77d98104d50af7a471d77ad3
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-04 19:26:45 -0700

    Time: 36 ms (87.97%) | Memory: 21.4 MB (37.03%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,11 +1,20 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        ans = -math.inf
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = -float('inf')
+        start = 0
         total = 0
+        result = ()
         for i in range(len(nums)):
             total+=nums[i]
-            if total>ans:
+            if total > ans:
                 ans = total
+                result = (start, i)
             if total<0:
                 total = 0
+                start = i + 1
         return ans
+                
