99013d0b8de71b75577216ed0599646812cad82f
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-04 21:29:39 -0700

    Time: 11 ms (20.90%) | Memory: 12.8 MB (8.77%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -5,13 +5,98 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        word_set = set(wordDict)  
-        dp = [False] * (len(s)+1)
-        dp[len(s)] = True
-        for i in range(len(s)-1, -1, -1):
-            for word in word_set:
-                if (i + len(word)) <= len(s) and s[i : i+len(word)] == word:
-                    dp[i] = dp [i+len(word)]
-                if dp[i]:
-                    break
-        return dp[0]
+        def recurse(start, s, memo):
+            if start == len(s):
+                return True
+            if memo[start] != -1:
+                if (memo[start] == 0):
+                    return False
+                
+                return True
+
+            for i in range(start, len(s)):
+                sub_s = s[start:i+1]
+                if sub_s in wordDict and recurse(i+1, s, memo):
+                    memo[start] = 1
+                    return True
+            
+            memo[start] = 0
+            return False
+
+        memo = [-1]*len(s)
+        return recurse(0, s, memo)
+
+
+        # for c in s:
+        #     sub_s+=c
+        #     if sub_s in wordDict:
+        #         sub_s = 0
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # word_set = set(wordDict)  
+        # dp = [False] * (len(s)+1)
+        # dp[len(s)] = True
+        # for i in range(len(s)-1, -1, -1):
+        #     for word in word_set:
+        #         if (i + len(word)) <= len(s) and s[i : i+len(word)] == word:
+        #             dp[i] = dp [i+len(word)]
+        #         if dp[i]:
+        #             break
+        # return dp[0]
