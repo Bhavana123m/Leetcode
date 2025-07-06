7c5a993d809288ce882739bc6f3f2c2ab0d2b243
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-06 15:57:59 -0700

    Time: 0 ms (100.00%) | Memory: 12.7 MB (25.20%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -7,23 +7,110 @@ class Solution(object):
         """
         rows = len(matrix)
         cols = len(matrix[0])
-        print(rows)
-        print(cols)
+        low = 0
+        high = (rows*cols)-1
+        while low<=high:
+            mid = (low+high)//2
+            row = mid//cols
+            col = mid%cols
+            val = matrix[row][col]
+            if target == val:
+                return True
+            elif val>target:
+                high-=1
+            else:
+                low+=1
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        rows = len(matrix)
+        cols = len(matrix[0])
         low = 0
         high = rows*cols -1
-        def binary_search(low, high):
-            if low > high:
-                return False 
+        while low<=high:
             mid = (low+high)//2
             row = mid // cols
             col = mid % cols
             mid_val = matrix[row][col]
             if mid_val == target:
                 return True
-            if mid_val>target:
+            elif mid_val < target:
+                low = mid + 1
+            else:
                 high = mid - 1
-            elif mid_val<target:
-                low = mid+1
-            return binary_search(low, high)
+
+        return False
+        # def binary_search(low, high):
+        #     if low > high:
+        #         return False 
+        #     mid = (low+high)//2
+        #     row = mid // cols
+        #     col = mid % cols
+        #     mid_val = matrix[row][col]
+        #     if mid_val == target:
+        #         return True
+        #     if mid_val>target:
+        #         high = mid - 1
+        #     elif mid_val<target:
+        #         low = mid+1
+        #     return binary_search(low, high)
             
-        return binary_search(low, high)
+        # return binary_search(low, high)
