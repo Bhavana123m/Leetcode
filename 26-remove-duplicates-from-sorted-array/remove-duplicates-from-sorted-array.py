10bb54b3fff4ef5f4fa851c237346bca1bbc51f1
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-05 22:54:22 -0700

    Time: 5 ms (32.95%) | Memory: 13.9 MB (36.38%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,12 +4,75 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        next = 1
+        end = 1
         for start in range(1, len(nums)):
             if nums[start]!=nums[start-1]:
-                nums[next] = nums[start]
-                next+=1
-        return next
+                nums[end] = nums[start]
+                end+=1
+        return end
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # next = 1
+        # for start in range(1, len(nums)):
+        #     if nums[start]!=nums[start-1]:
+        #         nums[next] = nums[start]
+        #         next+=1
+        # return next
 
         
         
@@ -21,16 +84,16 @@ class Solution(object):
         
         
         
-        count = 0
-        prev = []
-        for i in nums:
-            if i not in prev:
-                prev.append(i)
-                count+=1
-        nums[:] = prev
-        return count
+        # count = 0
+        # prev = []
+        # for i in nums:
+        #     if i not in prev:
+        #         prev.append(i)
+        #         count+=1
+        # nums[:] = prev
+        # return count
         
         
         
-        nums[:] = sorted(set(nums))
-        return len(nums)
+        # nums[:] = sorted(set(nums))
+        # return len(nums)
