b7a264e6b70cf3c20094d6694433b0072633c29f
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-07-13 16:19:50 -0700

    Time: 11 ms (72.43%) | Memory: 14.5 MB (15.65%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,119 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        left = 0
+        right = n-1
+        arr = [0]*n
+        for i in range(n-1, -1, -1):
+            if abs(nums[left])>abs(nums[right]):
+                arr[i] = nums[left]*nums[left]
+                left+=1
+            else:
+                arr[i] = nums[right]*nums[right]
+                right-=1
+        return arr
+
+
+        # left = 0 
+        # right = len(nums)-1
+
+        # while left<=right:
+        #     if abs(nums[left])<abs(nums[right]):
+        #         nums[right] = nums[right]*nums[right]
+        #         right-=1
+        #     elif abs(nums[left])>abs(nums[right]):
+        #         nums[left], nums[right] = nums[right], nums[left]
+        #         nums[right] = nums[right]*nums[right]
+        #         right-=1
+        #     elif abs(nums[left])==abs(nums[right]):
+        #         nums[right] = nums[right]*nums[right]
+        #         right-=1
+        # return nums
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        n = len(nums)
+        ans = [0] * n
+        start, end = 0, n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[start]) >= abs(nums[end]):
+                ans[i] = nums[start] * nums[start]
+                start += 1
+            else:
+                ans[i] = nums[end] * nums[end]
+                end -= 1
+        return ans
+        
