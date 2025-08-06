2569e4b4cfd638311bab99d6103be7c6cfc18797
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-08-06 01:31:18 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.46%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+        # https://leetcode.com/problems/plus-one/solutions/5360501/python-100-beat-100-efficient-optimal-solution-easy-to-understand
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i]+=1
+                return digits
+        return [1] + digits
+        
+        
         num_str = ""
         for digit in digits:
             num_str+=str(digit)
