1d23398b05a468db29a9af5c6043ced7e70435e7
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-08-06 13:36:32 -0700

    Time: 0 ms (100.00%) | Memory: 12.6 MB (24.16%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -5,6 +5,30 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        # Two Pointers
+        common = []
+        nums1_no_dup = list(set(nums1))
+        nums2_no_dup = list(set(nums2))
+        nums1_no_dup.sort()
+        nums2_no_dup.sort()
+
+        i = 0
+        j = 0
+        while i<len(nums1_no_dup) and j<len(nums2_no_dup):
+            if nums1_no_dup[i]<nums2_no_dup[j]:
+                i+=1
+            elif nums1_no_dup[i]>nums2_no_dup[j]:
+                j+=1
+            else:
+                common.append(nums1_no_dup[i])
+                i+=1
+                j+=1
+        return common
+
+        # inbuilt 
+        return list(set(nums1) & set(nums2))
+        
+        # Search in one set O(n)
         common = []
         nums1_no_dup = list(set(nums1))
         nums2_no_dup = list(set(nums2))
