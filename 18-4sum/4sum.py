4c0279162895f9002accde8337ec47c46f4684c2
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-08-05 17:13:43 -0700

    Time: 1774 ms (5.03%) | Memory: 12.6 MB (22.69%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -5,35 +5,49 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        ans = set()
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                ts = set()
+                for k in range(j+1, len(nums)):
+                    s = nums[i] + nums[j] + nums[k]
+                    fourth = target - s
+                    if(fourth in ts):
+                        ans.add(tuple(sorted([nums[i], nums[j], nums[k], fourth])))
+                    ts.add(nums[k])
+        return [list(t) for t in ans]
+
+        
         # https://leetcode.com/problems/4sum/solutions/6938935/brute-force-hashmap-two-pointers-visual-walkthroughs-java-python-code-dry-runs
-        result = []
-        n = len(nums)
-        if n<4:
-            return result
-        nums.sort()
-        for i in range(n-3):
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            for j in range(i+1, n-2):
-                if j > i+1 and nums[j] == nums[j-1]:
-                    continue
-                left = j + 1
-                right = n-1
-                while left<right:
-                    total = nums[left] + nums[right] + nums[i] + nums[j]
-                    if total == target:
-                        result.append([nums[i], nums[j], nums[left], nums[right]])
-                        while left<right and nums[left+1] == nums[left]:
-                            left+=1
-                        while left< right and nums[right-1] == nums[right]:
-                            right-=1
-                        left+=1
-                        right-=1
-                    elif total > target:
-                        right-=1
-                    else:
-                        left+=1
-        return result
+        # result = []
+        # n = len(nums)
+        # if n<4:
+        #     return result
+        # nums.sort()
+        # for i in range(n-3):
+        #     if i>0 and nums[i] == nums[i-1]:
+        #         continue
+        #     for j in range(i+1, n-2):
+        #         if j > i+1 and nums[j] == nums[j-1]:
+        #             continue
+        #         left = j + 1
+        #         right = n-1
+        #         while left<right:
+        #             total = nums[left] + nums[right] + nums[i] + nums[j]
+        #             if total == target:
+        #                 result.append([nums[i], nums[j], nums[left], nums[right]])
+        #                 while left<right and nums[left+1] == nums[left]:
+        #                     left+=1
+        #                 while left< right and nums[right-1] == nums[right]:
+        #                     right-=1
+        #                 left+=1
+        #                 right-=1
+        #             elif total > target:
+        #                 right-=1
+        #             else:
+        #                 left+=1
+        # return result
 
        
         # result = set()
