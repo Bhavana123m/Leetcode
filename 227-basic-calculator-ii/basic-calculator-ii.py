454d12c368c81e36a540589bbb2462321272fe88
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-08-18 15:05:39 -0700

    Time: 167 ms (49.35%) | Memory: 24 MB (20.50%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -4,58 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        def precedence(c):
-            return c == '*' or c == '/'
-
-        op, num, cur = deque(), deque(), 0
-        for c in s + '#':
-            if c == ' ': continue
-            elif c.isdigit():
-                cur = cur * 10 + int(c)
-            else:
-                num.append(cur)
-                while op and precedence(c) <= precedence(op[-1]):
-                    num1, num2, curOp = num.pop(), num.pop(), op.pop()
-                    if   curOp == '*': num.append(num2 * num1)
-                    elif curOp == '/': num.append(num2 // num1)
-                    elif curOp == '+': num.append(num2 + num1)
-                    elif curOp == '-': num.append(num2 - num1)
-                op.append(c)
-                cur = 0
-    
-        return num.pop()
-
-
-
-
-        def precedence(c):
-            return c == '*' or c == '/'
-        def toPostfix(s):
-            op, post = deque(), ''
-            for c in s:
-                if c == ' ': continue
-                elif c.isdigit(): post += c
-                else:
-                    post += '|'
-                    while op and precedence(c) <= precedence(op[-1]):
-                        post += op.pop()
-                    op.append(c)
-                    
-            return post + '|' + ''.join(reversed(op))
+        stack = []
+        num = 0
+        prev_op = '+'
+        n = len(s)
         
-        s, num, i = toPostfix(s), deque(), 0
-        while i < len(s):
+        for i in range(n):
             if s[i].isdigit():
-                j = s.find('|', i+1)
-                num.append(int(s[i:j]))
-                i = j
-            else:
-                num1, num2 = num.pop(), num.pop()
-                if   s[i] == '*': num.append(num2 * num1)
-                elif s[i] == '/': num.append(num2 // num1)
-                elif s[i] == '+': num.append(num2 + num1)
-                elif s[i] == '-': num.append(num2 - num1)
-            i += 1
-
-        return num.pop()
+                num = num * 10 + int(s[i])
+            if (not s[i].isdigit() and s[i] != ' ') or i == n - 1:
+                if prev_op == '+':
+                    stack.append(num)
+                elif prev_op == '-':
+                    stack.append(-num)
+                elif prev_op == '*':
+                    stack.append(stack.pop() * num)
+                elif prev_op == '/':
+                    # Handle division truncating toward zero
+                    top = stack.pop()
+                    if top // num < 0 and top % num != 0:
+                        stack.append(top // num + 1)
+                    else:
+                        stack.append(top // num)
+                prev_op = s[i]
+                num = 0
         
+        return sum(stack)
