3fc63e7bd6772ed9696cdc85d8f0a240a43751e9
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-08-19 00:33:50 -0700

    Time: 116 ms (98.37%) | Memory: 16.9 MB (53.90%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,32 +1,79 @@
 class Solution(object):
     def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        # Helper: integer division truncating toward 0
+        def div_trunc_zero(a, b):
+            q = abs(a) // abs(b)
+            return q if a * b >= 0 else -q
+
         stack = []
         num = 0
-        prev_op = '+'
+        sign = '+'
+
+        # Append sentinel operator to flush the last number
+        s = s.strip() + '+'
+        i = 0
         n = len(s)
-        
-        for i in range(n):
-            if s[i].isdigit():
-                num = num * 10 + int(s[i])
-            if (not s[i].isdigit() and s[i] != ' ') or i == n - 1:
-                if prev_op == '+':
+
+        while i < n:
+            ch = s[i]
+
+            if ch == ' ':
+                i += 1
+                continue
+
+            if '0' <= ch <= '9':
+                num = num * 10 + (ord(ch) - 48)
+            elif ch in '+-*/':
+                # Apply the previous sign with the number we've built
+                if sign == '+':
                     stack.append(num)
-                elif prev_op == '-':
+                elif sign == '-':
                     stack.append(-num)
-                elif prev_op == '*':
+                elif sign == '*':
                     stack.append(stack.pop() * num)
-                elif prev_op == '/':
-                    # Handle division truncating toward zero
-                    top = stack.pop()
-                    if top // num < 0 and top % num != 0:
-                        stack.append(top // num + 1)
-                    else:
-                        stack.append(top // num)
-                prev_op = s[i]
+                else:  # sign == '/'
+                    prev = stack.pop()
+                    stack.append(div_trunc_zero(prev, num))
+
+                # Update to the new sign and reset number
+                sign = ch
                 num = 0
-        
+            # (No other characters expected per problem statement)
+
+            i += 1
+
         return sum(stack)
+
+
+# class Solution(object):
+#     def calculate(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         stack = []
+#         num = 0
+#         prev_op = '+'
+#         n = len(s)
+        
+#         for i in range(n):
+#             if s[i].isdigit():
+#                 num = num * 10 + int(s[i])
+#             if (not s[i].isdigit() and s[i] != ' ') or i == n - 1:
+#                 if prev_op == '+':
+#                     stack.append(num)
+#                 elif prev_op == '-':
+#                     stack.append(-num)
+#                 elif prev_op == '*':
+#                     stack.append(stack.pop() * num)
+#                 elif prev_op == '/':
+#                     # Handle division truncating toward zero
+#                     top = stack.pop()
+#                     if top // num < 0 and top % num != 0:
+#                         stack.append(top // num + 1)
+#                     else:
+#                         stack.append(top // num)
+#                 prev_op = s[i]
+#                 num = 0
+        
+#         return sum(stack)
