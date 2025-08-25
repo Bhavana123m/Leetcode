d5381c5fcdab5ab3d6634bba4cb54238ca4b9039
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-08-25 14:02:44 -0700

    Time: 4 ms (85.64%) | Memory: 12.8 MB (99.72%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -4,6 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
+        output=[""]
+        for i in s:
+            temp=[]
+            if i.isalpha():
+                for o in output:
+                    temp.append(o+i.lower())
+                    temp.append(o+i.upper())
+            else:
+                for o in output:
+                    temp.append(o+i)
+            output=temp
+        return output
+
         n = len(s)
         if n == 0:
             return [""]
