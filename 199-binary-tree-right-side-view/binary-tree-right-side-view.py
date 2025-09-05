2ba9d5998eae8ff4af3493a90c5ed66ecea9bcde
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-09-04 21:51:54 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (93.23%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -10,7 +10,103 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[int]
         """
+        if not root:
+            return []
+        right_view = []
+        q = deque()
+        q.append(root)
+        while q:
+            q_len = len(q)
+            while q_len:
+                node = q.popleft()
+                if q_len == 1:
+                    right_view.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                q_len-=1
+        return right_view
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #top down perspective of the tree, as seen from "left to right"
+        #Get left view and right view of the tree and append the reveresed left view with the right_view[1:]
+        view = []
+        if not root:
+            return view
+        left_view = []
+        right_view = []
+        q = [root]
+        while q:
+            q_len = len(q)
+            level_len = q_len
+            while q_len:
+                node = q.pop(0)
+                if q_len == 1:
+                    left_view.append(node.val)
+                elif q_len == level_len:
+                    right_view.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                q_len-=1
+        # view = reversed(left_view)
+        left_view.reverse()
+        left_view.extend(right_view)
+        # left_
+        # for i in right_view:
+        #     view.append(i)
+        return left_view
+
         
+        
+        
+        #left view of the tree
+        view = []
+        if not root:
+            return view
+        q = [root]
+        while q:
+            q_len = len(q)
+            level_len = q_len
+            while q_len:
+                node = q.pop(0)
+                if level_len == q_len:
+                    view.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                q_len-=1
+        return view
+                
+       
+       # right side of the tree       
         view = []
         if not root:
             return view
