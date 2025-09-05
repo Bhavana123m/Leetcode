e23017468d20a4a4d70704e1203281963522dd71
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-09-04 22:52:13 -0700

    Time: 3 ms (36.47%) | Memory: 12.5 MB (88.09%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        subsets = []
+        subsets = set()
         def get_subsets(index, subsets, path):
-            add = path[:]
-            if add not in subsets:
-                subsets.append(add)
+            # add = path[:]
+            # if add not in subsets:
+            subsets.add(tuple(path[:]))
             for i in range(index, len(nums)):
                 path.append(nums[i])
                 get_subsets(i+1, subsets, path)
                 path.pop()            
         get_subsets(0, subsets, [])
-        return subsets
+        return list(subsets)
