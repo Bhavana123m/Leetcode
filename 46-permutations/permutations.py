3028664e8f98f99a69a419c31acf6e1cad25fabb
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-09-05 01:12:30 -0700

    Time: 1 ms (70.15%) | Memory: 12.8 MB (16.47%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -4,59 +4,122 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res = []
-        n = len(nums)
-        if n==1:
-            return [[nums[0]]]
-        for i in range(n):
-            num = nums.pop(i)
-            perm = self.permute(nums)
-            for each in perm:
-                each.append(num)
-            res+=perm
-            nums.insert(i,num)
-        return res
+        permutations = []
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        result = []
-        path = []
-        used = [False] * len(nums)
 
-        def backtrack():
+        def get_perm(permutaions, path):
             if len(path) == len(nums):
-                result.append(path[:])
+                permutations.append(path[:])
                 return
             for i in range(len(nums)):
-                if used[i]:
-                    continue
-                used[i] = True
-                path.append(nums[i])
-                backtrack()
-                path.pop()
-                used[i] = False
+                if nums[i] not in path:
+                    path.append(nums[i])
+                    get_perm(permutations, path)
+                    path.pop()
 
-        backtrack()
-        return result
+        get_perm(permutations, [])
+        return permutations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # res = []
+        # n = len(nums)
+        # if n==1:
+        #     return [[nums[0]]]
+        # for i in range(n):
+        #     num = nums.pop(i)
+        #     perm = self.permute(nums)
+        #     for each in perm:
+        #         each.append(num)
+        #     res+=perm
+        #     nums.insert(i,num)
+        # return res
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # result = []
+        # path = []
+        # used = [False] * len(nums)
+
+        # def backtrack():
+        #     if len(path) == len(nums):
+        #         result.append(path[:])
+        #         return
+        #     for i in range(len(nums)):
+        #         if used[i]:
+        #             continue
+        #         used[i] = True
+        #         path.append(nums[i])
+        #         backtrack()
+        #         path.pop()
+        #         used[i] = False
+
+        # backtrack()
+        # return result
