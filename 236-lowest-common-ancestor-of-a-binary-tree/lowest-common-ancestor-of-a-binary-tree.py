5ed1b3727ddb1048e66ccd96a119d6fe1b5a592a
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-09-08 15:45:18 -0700

    Time: 41 ms (92.56%) | Memory: 27.2 MB (19.08%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -13,19 +13,69 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        if root is None:
-            return None
-        if p.val == root.val or q.val == root.val:
-            return root
-        left_tree_common = self.lowestCommonAncestor(root.left, p, q)
-        right_tree_common = self.lowestCommonAncestor(root.right, p, q)
-        if left_tree_common and right_tree_common:
-            return root
-        if left_tree_common:
-            return left_tree_common
-        if right_tree_common:
-            return right_tree_common
+        # ------------------------------------ LCA I---------------------------------
+        # if root is None:
+        #     return None
+        # if p.val == root.val or q.val == root.val:
+        #     return root
+        # left_tree_common = self.lowestCommonAncestor(root.left, p, q)
+        # right_tree_common = self.lowestCommonAncestor(root.right, p, q)
+        # if left_tree_common and right_tree_common:
+        #     return root
+        # if left_tree_common:
+        #     return left_tree_common
+        # if right_tree_common:
+        #     return right_tree_common
 
+        # -------------------------------------LCA I(Simplified) -----------------------------------
+        
+        # if not root or root.val == p.val or root.val == q.val:
+        #     return root
+        # left = lowestCommonAncestor(root.left, p, q)
+        # right = lowestCommonAncestor(root.right, p, q)
+
+        # if left and right:
+        #     return root
+        # return left or right
+
+        # -------------------------------------LCA II(Simplified) -----------------------------------
+
+        if not root:
+            return None
+        found = [False, False]
+        # if p is q:
+        #      return p if exists(root, p) else None
+
+        
+        def dfs(node):
+            if not node:
+                return None
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if node is p:
+                found[0] = True
+                return node
+            if node is q:
+                found[1] = True
+                return node
+            if left and right:
+                return node
+            return left or right
+        
+        # def exists(root, target):
+        #     if not root:
+        #         return False
+        #     if root is target:
+        #         return True
+        #     return exists(root.left, target) or exists(root.right, target)
+        
+        
+        ancestor = dfs(root)
+        # print(ancestor)
+        # print(found)
+        if found[0] and found[1]:
+            return ancestor
+        return None
 
 
 
