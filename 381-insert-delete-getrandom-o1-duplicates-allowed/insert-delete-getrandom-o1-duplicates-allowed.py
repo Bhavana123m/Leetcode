de0d72edba61b3c032c944fcfcc33edd6718e8cb
Bhavana123m <147794111+Bhavana123m@users.noreply.github.com>
2025-09-27 22:26:03 -0700

    Time: 211 ms (44.03%) | Memory: 67 MB (82.84%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -1,31 +1,24 @@
 class RandomizedCollection(object):
 
     def __init__(self):
-        # Dynamic array to hold all elements, including duplicates
-        self.arr = []
-        # Hash map: value -> set of indices in self.arr where this value occurs
-        self.pos = {}
+        self.hashset = collections.defaultdict(set)
+        self.nums = []
+        
 
     def insert(self, val):
         """
         :type val: int
         :rtype: bool
         """
-        # Check if val already existed (non-empty set in pos)
-        existed = val in self.pos and len(self.pos[val]) > 0
+        if val in self.hashset:
+            self.nums.append(val)
+            self.hashset[val].add(len(self.nums) - 1)
+            return False
+        else:
+            self.nums.append(val)
+            self.hashset[val].add(len(self.nums) - 1)
+            return True
 
-        # If not existed before, initialize its set
-        if not existed:
-            self.pos.setdefault(val, set())
-
-        # Add the new index (tail of arr) into the set of positions
-        self.pos[val].add(len(self.arr))
-
-        # Append the value itself into the array
-        self.arr.append(val)
-
-        # Return True if this was the first time we saw val
-        return not existed
         
 
     def remove(self, val):
@@ -33,48 +26,24 @@ class RandomizedCollection(object):
         :type val: int
         :rtype: bool
         """
-        # If val absent or set empty, nothing to remove
-        if val not in self.pos or not self.pos[val]:
+        if val not in self.hashset:
             return False
-
-        # Get an arbitrary index where val occurs (e.g., pop one element from the set)
-        idx = self.pos[val].pop()
-
-        # Identify the last index and last value in the array
-        last_idx = len(self.arr) - 1
-        last_val = self.arr[last_idx]
-
-        # If the element to remove is not the last one, swap
-        if idx != last_idx:
-            # Move the last element into idx
-            self.arr[idx] = last_val
-
-            # Update last_val's index set:
-            # remove its old index (last_idx) and add the new index (idx)
-            self.pos[last_val].remove(last_idx)
-            self.pos[last_val].add(idx)
-
-        # Remove the last element from the array
-        self.arr.pop()
-
-        # Optional: clean up empty set for val
-        # if not self.pos[val]:
-        #     del self.pos[val]
-
-        return True
         
+        lastElement = self.nums[-1]
+        index = self.hashset[val].pop()
+
+        self.nums[index] = lastElement
+
+        self.hashset[lastElement].add(index)
+        self.hashset[lastElement].remove(len(self.nums) - 1)
+        self.nums.pop()
+
+        if not self.hashset[val]:
+            del self.hashset[val]
+        return True
 
     def getRandom(self):
         """
         :rtype: int
         """
-        # Choose a random index uniformly from the array
-        return self.arr[random.randint(0, len(self.arr) - 1)]
-        # Or simpler: return random.choice(self.arr)
-
-
-# Your RandomizedCollection object will be instantiated and called as such:
-# obj = RandomizedCollection()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        return random.choice(self.nums)
